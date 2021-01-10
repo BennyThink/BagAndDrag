0a18cb1528c7c5b462ce9cdfb5cc24561a733728
BennyThink <benny.think@gmail.com>
2021-01-10 17:30:05 +0800

    con

--- create_db.py
+++ create_db.py
@@ -12,7 +12,8 @@ import pymysql
 con = pymysql.Connect(host="127.0.0.1", user="root", password="root", charset="utf8mb4")
 
 sql = [
-    "CREATE DATABASE yyets CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;",
+    "CREATE DATABASE yyets CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;",
+    "use yyets",
     """
     create table resource
     (
